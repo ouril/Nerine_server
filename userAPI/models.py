d7f6a318895b3c4676939c03bf901d9d2a710000
ouril <ourilkregg@gmail.com>
2017-08-10 12:58:06 +0300

    добавлен requirements.txt

--- userAPI/models.py
+++ userAPI/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Persons(models.Model):
+    name = models.CharField(max_length=2048, unique=True)
+    
+class Sites(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+
+class Pages(models.Model):
+    
+
+class PersonPageRank(models.Model):
+    pass
